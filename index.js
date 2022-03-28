f742664549b43e7db3b86f7a91f3d01ea31826a7
mpramos <mpramos78915400@gmail.com>
2022-03-27 21:01:33 -0400

    deployment

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 const { logErrors,errorHandler,boomErrorHandler}=require('./middlewares/error.handler')
 const app = express();
 const port = 3000;
 app.use(express.json());
-
+const whitelist=['http://localhost:8080','http://myapp.co'];
+const options ={
+  origin :(origin,callback)=>{
+      if(whitelist.includes(origin)!==1){
+        callback(null,true);
+      }else{
+        callback(new Error('no permitido'))
+      }
+  }
+}
+//acepta cualquier origen
+app.use(cors(options));
 routerApi(app);
 app.use(logErrors);
 app.use(boomErrorHandler);
